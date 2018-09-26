2364889be341820e7470d0f53e6efba58e8be93c
Abdallah Samman <sammanabdallah@gmail.com>
2018-09-26 03:59:35 +0000

    turn heat-index into a module

--- lib/heat-index.rb
+++ lib/heat-index.rb
@@ -1,83 +1,85 @@
-class HeatIndex
-  def initialize(temperature: nil, humidity: nil)
-	@temperature = temperature   # temperature degree
-	@humidity = humidity         # percentage e.g. 0 < @humidity < 100
+module HeatIndex
+  class HeatIndex
+    def initialize(temperature: nil, humidity: nil)
+      @temperature = temperature   # temperature degree
+      @humidity = humidity         # percentage e.g. 0 < @humidity < 100
+    end
+
+    def calculate
+      # Creating multiples of 'fahrenheit' & 'hum' values for the coefficients
+      f = @temperature.as_f.value
+      t = f ** 2
+      h = @humidity ** 2
+
+      # Coefficients for the calculations
+      c = [-42.379, 2.04901523, 10.14333127, -0.22475541, -6.83783e-03, -5.481717e-02, 1.22874e-03, 8.5282e-04, -1.99e-06]
+
+      # Calculating heat-index
+      heatindex =
+          c[0] +
+          (c[1] * f) +
+          (c[2] * @humidity) +
+          (c[3] * f * @humidity) +
+          (c[4] * t) + (c[5] * h) +
+          (c[6] * t * @humidity) +
+          (c[7] * f * h) +
+          (c[8] * t * h)
+
+      Fahrenheit.new(heatindex)
+    end
   end
 
-  def calculate
-	# Creating multiples of 'fahrenheit' & 'hum' values for the coefficients
-	f = @temperature.as_f.value
-	t = f ** 2
-	h = @humidity ** 2
+  class Degree
+    attr_accessor :value
 
-	# Coefficients for the calculations
-	c = [-42.379, 2.04901523, 10.14333127, -0.22475541, -6.83783e-03, -5.481717e-02, 1.22874e-03, 8.5282e-04, -1.99e-06]
+    def initialize(value)
+      self.value = value
+    end
 
-	# Calculating heat-index
-	heatindex =
-		c[0] +
-		(c[1] * f) +
-		(c[2] * @humidity) +
-		(c[3] * f * @humidity) +
-		(c[4] * t) + (c[5] * h) +
-		(c[6] * t * @humidity) +
-		(c[7] * f * h) +
-		(c[8] * t * h)
-
-	Fahrenheit.new(heatindex)
-  end
-end
-
-class Degree
-  attr_accessor :value
-
-  def initialize(value)
-  	self.value = value
+    def rounded_value
+      value.round
+    end
   end
 
-  def rounded_value
-    value.round
+  class Celsius < Degree
+      def as_f
+          Fahrenheit.new(value * (9.0 / 5.0) + 32)
+      end
+
+      def as_c
+          self
+      end
+
+      def as_k
+          Kelvin.new(value + 273)
+      end
   end
-end
 
-class Celsius < Degree
-	def as_f
-		Fahrenheit.new(value * (9.0 / 5.0) + 32)
-	end
+  class Kelvin < Degree
+      def as_f
+          self.as_c.as_f	
+      end
 
-	def as_c
-		self
-	end
+      def as_c
+          Celsius.new(value - 273)
+      end
 
-	def as_k
-		Kelvin.new(value + 273)
-	end
-end
+      def as_k
+          self
+      end
+  end
 
-class Kelvin < Degree
-	def as_f
-		self.as_c.as_f	
-	end
+  class Fahrenheit < Degree
+      def as_f
+          self
+      end
 
-	def as_c
-		Celsius.new(value - 273)
-	end
+      def as_c
+          Celsius.new((5 * (value - 32)) / 9)
+      end
 
-	def as_k
-		self
-	end
-end
-
-class Fahrenheit < Degree
-	def as_f
-		self
-	end
-
-	def as_c
-		Celsius.new((5 * (value - 32)) / 9)
-	end
-
-	def as_k
-		self.as_c.as_k
-	end
+      def as_k
+          self.as_c.as_k
+      end
+  end
 end
